1f97d10e905ec3aa17f4fe89d7254388731e2b0d
jflinn18atWhitworth <jflinn18@my.whitworth.edu>
2016-04-28 08:42:45 -0700

    Graded HW6
    EX06_01: Nice Work
    EX06_02: Great job
    EX06_03: No UML, but other than, looks great
    EX06_04: Some extra code that inhibits the purpose for the program
    
    Extra Credit
    EX06_05: None

--- HW6/EX06_04 Shuffle Vector/EX06_04 Shuffle Vector/ShuffleVector.cpp
+++ HW6/EX06_04 Shuffle Vector/EX06_04 Shuffle Vector/ShuffleVector.cpp
@@ -16,7 +16,7 @@ int main() {
 	cout << "The 10 int values from 1 - 10 randomized are: "<<endl;
 	shuffle(values);
 	for (int i = 0; i < values.size(); i++) {
-		cout << values[i] +1 << endl;
+		cout << values[i] + 1 << endl;				// Joseph: I don't quite understand why there is a +1 here. If it was removed, than this program could shuffle any T type objects, even Cats.
 	}
 	return 0;
 }
@@ -24,7 +24,8 @@ int main() {
 template <typename T>
 void shuffle(vector<T>& v)
 {
-	for (int i = 0; i < v.size(); i++)
+	for (int i = 0; i < v.size(); i++)  // Joseph: This will now only work for integers because 'i' is an integer
+										//         Without this loop, the function should work perfectly.
 	{
 		v[i] = i;
 	}
@@ -32,7 +33,7 @@ void shuffle(vector<T>& v)
 	srand(time(0));
 	for (int i = 0; i < v.size(); i++)
 	{
-		int index = rand() % v.size();
+		int index = rand() % v.size();  
 		int temp = v[i];
 		v[i] = v[index];
 		v[index] = temp;
